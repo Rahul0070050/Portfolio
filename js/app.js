15f0d08abe6bb870ec3a272d0dfd6d533c5c7e9e
Rahul <rahulor2000@gmail.com>
2021-01-01 06:36:50 +0530

    i mised somthing! this is new update

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".burgur").click(function(){
+        $(".navbar>.links").toggleClass("display-Block");
+    })
+    $("ul>li>a").click(function(){
+        if($(".navbar>.links").hasClass("display-Block")){
+            $(".navbar>.links").removeClass("display-Block")
+        }
+    })
+    $("section").click(function(){
+        if($(".navbar>.links").hasClass("display-Block")){
+            $(".navbar>.links").removeClass("display-Block")
+        }
+    })
+})
+
+const loder = document.getElementById("loader");
+window.addEventListener("load", function(){
+    loder.classList.add("fadeout");
+});
